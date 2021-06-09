db31f72b05335eddfdc289bb8b07c7b68b52bfb2
agesa3 <collinsagesa2000@gmail.com>
2021-06-10 02:21:36 +0300

    finalized in adding the form

--- controllers/courses.js
+++ controllers/courses.js
@@ -4,12 +4,31 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const CourseModel = mongoose.model("Course");
 
+router.get("/add", (req, res) => {
+  res.render("add_course");
+});
+
+router.post("/add", (req, res) => {
+  var course = new CourseModel();
+  course.courseName = req.body.courseName;
+  course.courseDuration = req.body.courseDuration;
+  course.courseFees = req.body.courseFees;
+  course.courseId = Math.ceil(Math.random() * 1000000)+"";
+  course.save((err, doc) => {
+    if (!err) {
+      res.redirect("/courses/list");
+    } else {
+      res.send("Error occured");
+    }
+  });
+});
+
 router.get("/list", (req, res) => {
-//   var course = new CourseModel();
-//   course.courseName = "Node Js";
-//   course.courseId = "2";
-//   course.save();
- 
+  //   var course = new CourseModel();
+  //   course.courseName = "Node Js";
+  //   course.courseId = "2";
+  //   course.save();
+
   CourseModel.find({})
     .lean()
     .exec(function (err, docs) {
